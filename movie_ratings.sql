b3bbf1e7a3878a891246973b25edbf884b97a2d4
Mariam Tencio <122467946+MariamTencio@users.noreply.github.com>
2024-07-24 09:21:02 -0600

    Update movie_ratings.sql

--- movie_ratings.sql
+++ movie_ratings.sql
@@ -13,12 +13,14 @@ CREATE TABLE Movies (
     director TEXT
 );
 
+
 -- Create Users table
 CREATE TABLE Users (
     id INTEGER PRIMARY KEY,
     username TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE
 );
+
 
 -- Create Ratings table
 CREATE TABLE Ratings (
@@ -36,26 +38,32 @@ CREATE TABLE Ratings (
 INSERT INTO Movies (id, title, genre, release_date, director)
 VALUES (1, 'The Dark Knight', 'Action/Crime', '2008-07-18', 'Christopher Nolan');
 
+
 -- Insert a new user
 INSERT INTO Users (id, username, email)
 VALUES (1, 'mariamtencio', 'mariamtch412@gmail.com');
 
+
 -- Insert a new rating
 INSERT INTO Ratings (id, user_id, movie_id, rating, review, rating_date)
 VALUES (1, 1, 1, 5, 'Favorite movie!', '2024-07-22');
+
 
 -- Update the rating of a specific review
 UPDATE Ratings
 SET rating = 4, review = 'Great movie!'
 WHERE id = 1;
 
+
 -- Delete a rating by id
 DELETE FROM Ratings
 WHERE id = 1;
 
+
 -- Retrieve ratings from a specific user
 SELECT * FROM Ratings
 WHERE user_id = 1;
+
 
 -- Retrieve movie titles and their average ratings using a join
 SELECT Movies.title, AVG(Ratings.rating) AS average_rating
